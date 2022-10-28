78df5b246fe05540f11319352d6620ceabf27e79
YunusOrak <yunusorak08@hotmail.com>
2022-10-28 12:41:06 +0300

    d

--- wp-content/themes/arc-fse/inc/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Bootstrap file to initiate core files.
- */
-include 'core/styles.php';
-include 'core/pattern-category.php';
-include 'core/template-functions.php';
-include 'core/theme-info.php';
